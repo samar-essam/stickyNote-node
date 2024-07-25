98611df4ab9589f6a2fdefa3f9889b7951096e0b
samar-essam <samaresam243@gmail.com>
2024-07-25 16:30:10 +0300

    third commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import { dbConnections } from './DB/dbConnection.js'
 import userRouter from './src/modules/user/user.routes.js'
 import noteRouter from './src/modules/note/note.routes.js'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
-
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(noteRouter)
