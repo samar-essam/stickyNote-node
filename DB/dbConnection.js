0dcaf0f014e263d64f5c92486d6ad03d21df6b0c
samar-essam <samaresam243@gmail.com>
2024-07-25 16:38:29 +0300

    updated commit

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export function dbConnections(){
-    mongoose.connect('mongodb+srv://samarEssam:gQVCo8HX6ouygB0o@cluster0.6hiiuac.mongodb.net/sara7a-app/stickyNote').then( ()=> {
+    mongoose.connect('mongodb+srv://samarEssam:gQVCo8HX6ouygB0o@cluster0.6hiiuac.mongodb.net/stickyNote').then( ()=> {
         console.log("DB is connected ");
     }).catch((err)=>{
         console.log("error connection");
